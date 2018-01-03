507945a40d1d34fe2e1ce4782b563063c48ded27
DESKTOP-PT20SU4\Chang <msps50807@gmail.com>
2018-01-03 12:44:37 +0800

    test

---
+++ src/Test/letterGrade.java
@@ -0,0 +1,25 @@
+package Test;
+
+import java.util.Scanner;
+
+public class letterGrade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println(letterGrade(scanner.nextInt()));
+		}
+	}
+	
+	public static char letterGrade(int score) { char grade;
+	if (score <0 || score > 100) grade = 'X';
+	else if (score>=90 && score <=100) grade = 'A';
+	else if (score>=80 && score <90) grade = 'B';
+	else if (score>=70 && score <80) grade = 'C';
+	else if (score>=60 && score <70) grade = 'D';
+	else
+	grade ='F'; return grade;
+	}
+
+}
